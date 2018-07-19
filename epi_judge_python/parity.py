ebd83510eaa97a7d4acb10308a850de15000df3f
Alex Keyes <alex@signafire.com>
2018-07-19 11:42:12 -0400

    brute force solution for parity

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def parity(x):
-    # TODO - you fill in here.
-    return 0
+    """ Compute the parity of a word, returning 1 if it's bits are odd, 0 if it's even """
+    # okay we need to count the number of bits and then return 1 if the count is odd, otherwise return false
+    bit_count = 0
+    while x:
+        bit_count += x & 1
+        x >>= 1
+
+    if bit_count % 2 == 0:
+        return 0
+    else:
+        return 1
 
 
 if __name__ == '__main__':
