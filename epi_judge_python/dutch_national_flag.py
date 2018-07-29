4c65d3102021ceb1264ea71b34605971eb2c3707
Alex Keyes <alex@signafire.com>
2018-07-29 11:57:20 -0400

    start array problems

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -8,8 +8,27 @@ RED, WHITE, BLUE = range(3)
 
 
 def dutch_flag_partition(pivot_index, A):
-    # TODO - you fill in here.
-    return
+    """Write a program that takes an array A and an index i into A,
+    and rearranges the elements such that all elements less than A[i] (the “pivot”) appear first,
+    followed by elements equal to the pivot, followed by elements greater than the pivot."""
+    pivot_val = A[pivot_index]
+    low = 0
+    iterator = 0
+    high = len(A) - 1
+
+    while iterator <= high:
+        if A[iterator] < pivot_val:
+            # if A iter is less than pivot swap A low and A iterator, increment both
+            A[low], A[iterator] = A[iterator], A[low]
+            low += 1
+            iterator += 1
+        elif A[iterator] > pivot_val:
+            # if greater than, swap A iter and A high
+            A[iterator], A[high] = A[high], A[iterator]
+            high -= 1
+        else:
+            iterator += 1
+    return A
 
 
 @enable_executor_hook
@@ -31,7 +50,8 @@ def dutch_flag_partition_wrapper(executor, A, pivot_idx):
     while i < len(A) and A[i] > pivot:
         count[A[i]] -= 1
         i += 1
-
+    # print(i)
+    # print(len(A))
     if i != len(A):
         raise TestFailure('Not partitioned after {}th element'.format(i))
     elif any(count):
