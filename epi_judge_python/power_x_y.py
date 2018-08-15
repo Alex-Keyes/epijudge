de86b0684d99c97dac1aef176cdd4de5a636d69b
Alex Keyes <alex@signafire.com>
2018-08-14 22:53:19 -0400

    add in solutions

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -1,11 +1,17 @@
 from test_framework import generic_test
 
 
-def power(x, y):
-    # TODO - you fill in here.
-    return 0.0
+def power_x_y(x, y):
+    result, power = 1.0, y
+    if y < 0:
+        power, x = -power, 1.0 / x
+    while power:
+        if power & 1:
+            result *= x
+        x, power = x * x, power >> 1
+    return result
 
 
 if __name__ == '__main__':
     exit(
-        generic_test.generic_test_main("power_x_y.py", 'power_x_y.tsv', power))
+        generic_test.generic_test_main("power_x_y.py", 'power_x_y.tsv', power_x_y))
--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
+# write a function that reversed the digit of an integer passed to it
+"""
+Q: why don't we convert the integer to a string and then return the reversed string?
+A: no dice, that doesn't work for negative test cases...
+Q: hah no shit that worked. 
+A: that being said, it's probably a bit faster to do something a bit faster than how this works
+
+maybe we can see if the string input thing works
+
+interesting that the 'faster' algorithm isn't actually much faster
+"""
+
 
 def reverse(x):
-    # TODO - you fill in here.
-    return 0
+    str_x = str(x)
+    if str_x[0] == '-':
+        rev_str = str_x[:0:-1]
+        rev_str = '-' + rev_str
+    else:
+        rev_str = str_x[::-1]
+    return int(rev_str)
 
 
 if __name__ == '__main__':
