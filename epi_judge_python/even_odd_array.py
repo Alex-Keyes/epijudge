a8a9577d2236a259a42be607bef4d430c13cfec9
Alex Keyes <alex@signafire.com>
2018-08-14 23:21:08 -0400

    complete e

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -4,11 +4,22 @@ import functools
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
+"""
+okay I need to handle 3 arrays
+the first thing I need to do is setup the 3 arrays
+then iterate through the array, putting the elements of unclassified into either the even or odd array
+once that whole process is done, we can return an array where it's even then odd
+"""
 
 
 def even_odd(A):
-    # TODO - you fill in here.
-    return
+    next_even, next_odd = 0, len(A) - 1
+    while next_even < next_odd:
+        if A[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -= 1
 
 
 @enable_executor_hook
